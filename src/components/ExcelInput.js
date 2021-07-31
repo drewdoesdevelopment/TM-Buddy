51aa8e6ecaafd0a5a9c011ec604c3b516080f66f
Drew Develops <drewcpfeiler@gmail.com>
2021-07-30 20:52:01 -0500

    + Attendance Action Button
    Added the 'Attendace' action button and implemented the functionality. Additionally cleaned line breaks.

--- src/components/ExcelInput.js
+++ src/components/ExcelInput.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Dropdown } from 'react-bootstrap';
 import { PersonFill } from 'react-bootstrap-icons';
 import './ExcelInput.scss';
 import * as XLSX from 'xlsx';
 
 const ExcelInput = () => {
   const [employeeName, setEmployeeName] = useState([]);
+  const [employeeEmail, setEmployeeEmail] = useState([]);
 
   const handleExcelInput = (e) => {
     const readExcel = (file) => {
@@ -15,13 +16,9 @@ const ExcelInput = () => {
 
         fileReader.onload = (e) => {
           const bufferArray = e.target.result;
-
           const wb = XLSX.read(bufferArray, { type: 'buffer' });
-
           const wsName = wb.SheetNames[0];
-
           const ws = wb.Sheets[wsName];
-
           const data = XLSX.utils.sheet_to_json(ws);
           resolve(data);
         };
@@ -42,8 +39,10 @@ const ExcelInput = () => {
   };
 
   return (
-    <>
-      <input type='file' placeholder='fart' onChange={handleExcelInput} />
+    <div className='team__dashboard'>
+      <Container className='team__input'>
+        <input type='file' placeholder='fart' onChange={handleExcelInput} />
+      </Container>
       <Container className='card__container'>
         {employeeName.map((employee) => {
           return (
@@ -58,12 +57,43 @@ const ExcelInput = () => {
                 <Card.Subtitle className='mb-2 text-muted'>
                   Contact: {employee.PhoneNumber}
                 </Card.Subtitle>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  Email: {employee.EmployeeEmail}
+                </Card.Subtitle>
+                <Dropdown className='pt-2'>
+                  <Dropdown.Toggle variant='dark' id='dropdown-basic'>
+                    Attendance
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu variant='dark'>
+                    <Dropdown.Item
+                      onClick={() =>
+                        window.open(
+                          `mailto:${employee.EmployeeEmail}?subject=Absence - ${
+                            employee.Employee
+                          }&body=Hello ${
+                            employee.Employee.split(' ')[0]
+                          }, it looks like you've called out for your shift today. I hope all is well, when can we be expecting you back?`
+                        )
+                      }
+                      href='#/action-1'
+                    >
+                      Mark Absent
+                    </Dropdown.Item>
+                    <Dropdown.Item href='#/action-2'>
+                      Send RTW Notice
+                    </Dropdown.Item>
+                    <Dropdown.Item href='#/action-3'>
+                      Generate Travel Request
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
               </Card.Body>
             </Card>
           );
         })}
       </Container>
-    </>
+    </div>
   );
 };
 
